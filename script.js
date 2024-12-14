e9f6de1025f9ee3b98c8bca56faeb585f92644c2
Ruisu <joseluisosorioguzman11@gmail.com>
2024-12-13 19:19:38 -0500

    Img Responsive

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const enlaces = nav.querySelectorAll("a"); // Seleccionamos todos los enlaces dentro del menú
 
+// Abrir menú
 abrir.addEventListener("click", () => {
   nav.classList.add("visible");
 });
 
+// Cerrar menú
 cerrar.addEventListener("click", () => {
   nav.classList.remove("visible");
 });
 
+// Cerrar menú automáticamente al hacer clic en un enlace
+enlaces.forEach((enlace) => {
+  enlace.addEventListener("click", () => {
+    nav.classList.remove("visible");
+  });
+});
+
 let sliderInner = document.querySelector(".slider--inner");
-let images = sliderInner.querySelectorAll("img"); // Selecciona todas las imágenes
+let imagess = sliderInner.querySelectorAll("img"); // Selecciona todas las imágenes
 let index = 0; // Empieza desde la primera imagen
 
 setInterval(function () {
   let percentage = index * -100;
   sliderInner.style.transform = "translateX(" + percentage + "%)";
   index++;
-  if (index >= images.length) { // Cambia la condición
-    index = 0; // Vuelve al inicio
+  if (index >= imagess.length) {
+    index = 0;
   }
 }, 1000);
-
